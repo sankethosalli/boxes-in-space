84d198fab7703d0181c65102de38c53437bed4cd
shosalli <shosalli220599@gmail.com>
2022-03-16 21:40:21 +0530

    regular update

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import Graphic from "./components/Graphic";
+
+class App extends Component {
+  render() {
+    return (
+      // <div>
+      <Graphic />
+      // </div>
+    );
+  }
+}
+
+export default App;
