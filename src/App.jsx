2488302d2f8b2f9b43aa5a7a69f33c389bb5997f
shosalli <shosalli220599@gmail.com>
2022-03-18 15:28:01 +0530

    regular update

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import React, { Component } from "react";
-import Graphic from "./components/Graphic";
+// import Graphic from "./components/Graphic";
+// import Wobble from "./components/Wobble";
+import Pmnd from "./components/Pmnd";
 
 class App extends Component {
   render() {
     return (
       // <div>
-      <Graphic />
+      // <Graphic />
+      // <Wobble />
+      <Pmnd />
       // </div>
     );
   }
