4dd0bb811171be1559126f5c8d5797aef97f3ff4
shosalli <shosalli220599@gmail.com>
2022-03-19 20:13:46 +0530

    regular update

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -1,21 +1,59 @@
-import React from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { Canvas } from "react-three-fiber";
 import { OrbitControls, Stars } from "drei";
 import { Physics, usePlane, useBox } from "use-cannon";
+import { useSpring, animated, a } from "react-spring";
 import "./styles.css";
 
-function Box() {
-  //   const [ref, api] = useBox(() => ({ mass: 1, position: [0, 2, 0] }));
+const COLORS = [
+  "black",
+  "silver",
+  "gray",
+  "white",
+  "maroon",
+  "red",
+  "purple",
+  "fuchsia",
+  "green",
+  "lime",
+  "olive",
+  "yellow",
+  "navy",
+  "blue",
+  "teal",
+  "aqua",
+];
+
+function Box({ position }) {
+  const [hovered, setHovered] = useState(false);
+  const [active, setActive] = useState(false);
+  const props = useSpring({
+    scale: active ? [0.8, 0.8, 0.8] : [0.4, 0.4, 0.4],
+    color: hovered ? COLORS[Math.floor(Math.random() * COLORS.length)] : "grey",
+  });
+
+  const [ref, api] = useBox(() => ({ mass: 0, position: position }));
+
+  const handleClick = () => {
+    setActive(() => !active);
+  };
+
   return (
     <mesh
-      //   onClick={() => {
-      //     api.velocity.set(0, 6, 4);
-      //   }}
-      //   ref={ref}
-      position={[0, 0, 0]}
+      onClick={handleClick}
+      ref={ref}
+      position={position}
+      onPointerOver={() => setHovered(true)}
+      onPointerOut={() => setHovered(false)}
+      scale={active ? [0.8, 0.8, 0.8] : [0.4, 0.4, 0.4]}
     >
       <boxBufferGeometry attach="geometry" />
-      <meshLambertMaterial attach="material" color="hotpink" />
+      <meshLambertMaterial
+        attach="material"
+        color={
+          hovered ? COLORS[Math.floor(Math.random() * COLORS.length)] : "grey"
+        }
+      />
     </mesh>
   );
 }
@@ -33,17 +71,23 @@ function Plane() {
   );
 }
 
-export default function Graphic() {
+export default function Task() {
+  const [boxes, setBoxes] = useState(() => []);
   return (
     <Canvas>
-      {/* <OrbitControls /> */}
+      <OrbitControls />
       <Stars />
       <ambientLight intensity={0.5} />
       <spotLight position={[10, 15, 10]} angle={0.3} />
-      {/* <Physics> */}
-      <Box />
-      <Plane />
-      {/* </Physics>  */}
+
+      <Physics>
+        {/* <Plane /> */}
+        <Box position={[0, 0, 0]} />
+        <Box position={[0, 2, 0]} />
+        <Box position={[2, 0, 0]} />
+        <Box position={[2, 2, 0]} />
+        <Box position={[2, 4, 0]} />
+      </Physics>
     </Canvas>
   );
 }
