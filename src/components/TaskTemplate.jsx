fa64bd95c16b65ade74234acc5a76f1a6767b79a
shosalli <shosalli220599@gmail.com>
2022-03-20 07:16:54 +0530

    regular update

--- src/components/TaskTemplate.jsx
+++ src/components/TaskTemplate.jsx
@@ -2,6 +2,8 @@ import React, { useState, useEffect, useRef, useContext } from "react";
 import * as THREE from "three";
 import { Canvas, extend, useThree, useFrame } from "react-three-fiber";
 import { OrbitControls, Stars } from "drei";
+// import { Stars } from "drei";
+// import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 
 import { Physics, usePlane, useBox } from "use-cannon";
 import { useSpring, animated, a } from "react-spring";
@@ -35,8 +37,33 @@ function Box({ position, setBox }) {
     setBox(() => meshRef);
   };
 
+  // Rotation Animation
+  // useFrame(() => {
+  //   meshRef.current.rotation.y += 0.01;
+  // });
+
+  // useEffect(() => {
+  //   meshRef.current.rotation.x += Math.floor(
+  //     Math.random() * configLocal.ROTATE_MAX
+  //   );
+  //   meshRef.current.rotation.y += Math.floor(
+  //     Math.random() * configLocal.ROTATE_MAX
+  //   );
+  //   meshRef.current.rotation.z += Math.floor(
+  //     Math.random() * configLocal.ROTATE_MAX
+  //   );
+  // });
+
   return (
-    <mesh onClick={handleClick} ref={meshRef} position={position}>
+    <mesh
+      onClick={handleClick}
+      ref={meshRef}
+      position={position}
+      // onPointerOver={() => setHovered(true)}
+      // onPointerOut={() => setHovered(false)}
+      // scale={active ? [0.8, 0.8, 0.8] : [0.4, 0.4, 0.4]}
+      // scale={[getActiveRange(), getActiveRange(), getActiveRange()]}
+    >
       <boxBufferGeometry attach="geometry" />
       <meshLambertMaterial
         attach="material"
@@ -74,6 +101,8 @@ export default function Task() {
 
         <div className={"col-10 " + styles.Canvas}>
           <Canvas camera={{ position: [0, 0, 5] }}>
+            {/* <OrbitControls /> */}
+
             <ambientLight intensity={0.5} />
             <spotLight position={[10, 15, 10]} angle={0.3} penumbra={1} />
 
---
+++ src/components/Transform.jsx
@@ -0,0 +1,184 @@
+import React, { useReducer } from "react";
+import styles from "./Transform.module.scss";
+
+const ACTIONS = {
+  ROTATE: {
+    LEFT: "LEFT",
+    RIGHT: "RIGHT",
+
+    UP: "UP",
+    DOWN: "DOWN",
+  },
+  SCALE: {
+    SHRINK: "SHRINK",
+    ENLARGE: "ENLARGE",
+  },
+  TRANSLATE: {
+    LEFT: "LEFT",
+    RIGHT: "RIGHT",
+
+    UP: "UP",
+    DOWN: "DOWN",
+  },
+};
+
+export default function Transform({ box }) {
+  function rotate(action) {
+    switch (action) {
+      case ACTIONS.ROTATE.LEFT:
+        box.current.rotation.y -= 0.1;
+        return null;
+      case ACTIONS.ROTATE.RIGHT:
+        box.current.rotation.y += 0.1;
+        return null;
+      case ACTIONS.ROTATE.UP:
+        box.current.rotation.x -= 0.1;
+        return null;
+      case ACTIONS.ROTATE.DOWN:
+        box.current.rotation.x += 0.1;
+        return null;
+      default:
+        return null;
+    }
+  }
+
+  function scale(action) {
+    switch (action) {
+      case ACTIONS.SCALE.SHRINK:
+        box.current.scale.x -= 0.1;
+        box.current.scale.y -= 0.1;
+        box.current.scale.z -= 0.1;
+        return null;
+      case ACTIONS.SCALE.ENLARGE:
+        box.current.scale.x += 0.1;
+        box.current.scale.y += 0.1;
+        box.current.scale.z += 0.1;
+        return null;
+      default:
+        return null;
+    }
+  }
+
+  function translate(action) {
+    switch (action) {
+      case ACTIONS.TRANSLATE.LEFT:
+        box.current.position.x -= 0.1;
+        return null;
+      case ACTIONS.TRANSLATE.RIGHT:
+        box.current.position.x += 0.1;
+        return null;
+      case ACTIONS.TRANSLATE.UP:
+        box.current.position.y += 0.1;
+        return null;
+      case ACTIONS.TRANSLATE.DOWN:
+        box.current.position.y -= 0.1;
+        return null;
+      default:
+        return null;
+    }
+  }
+
+  return (
+    <div className={styles.Transform}>
+      <div className={styles.Header}>
+        {box ? (
+          <h1>Transform</h1>
+        ) : (
+          <>
+            <h1>Transform</h1>
+            <h6>(Click On Box)</h6>
+          </>
+        )}
+      </div>
+
+      {box ? (
+        <div className={styles.Controls}>
+          <div className={styles.Section}>
+            <h2>Rotate</h2>
+            <div>
+              <button
+                className="btn btn-primary"
+                onClick={() => rotate(ACTIONS.ROTATE.LEFT)}
+              >
+                Left
+              </button>
+              &nbsp;
+              <button
+                className="btn btn-primary"
+                onClick={() => rotate(ACTIONS.ROTATE.RIGHT)}
+              >
+                Right
+              </button>
+            </div>
+            <div>
+              <button
+                className="btn btn-primary"
+                onClick={() => rotate(ACTIONS.ROTATE.UP)}
+              >
+                Up
+              </button>
+              &nbsp;
+              <button
+                className="btn btn-primary"
+                onClick={() => rotate(ACTIONS.ROTATE.DOWN)}
+              >
+                Down
+              </button>
+            </div>
+          </div>
+
+          <div className={styles.Section}>
+            <h2>Scale</h2>
+            <button
+              className="btn btn-primary"
+              onClick={() => scale(ACTIONS.SCALE.SHRINK)}
+            >
+              Shrink
+            </button>
+            &nbsp;
+            <button
+              className="btn btn-primary"
+              onClick={() => scale(ACTIONS.SCALE.ENLARGE)}
+            >
+              Enlarge
+            </button>
+          </div>
+
+          <div className={styles.Section}>
+            <h2>Translate</h2>
+            <div>
+              <button
+                className="btn btn-primary"
+                onClick={() => translate(ACTIONS.TRANSLATE.LEFT)}
+              >
+                Left
+              </button>
+              &nbsp;
+              <button
+                className="btn btn-primary"
+                onClick={() => translate(ACTIONS.TRANSLATE.RIGHT)}
+              >
+                Right
+              </button>
+            </div>
+            <div>
+              <button
+                className="btn btn-primary"
+                onClick={() => translate(ACTIONS.TRANSLATE.UP)}
+              >
+                Up
+              </button>
+              &nbsp;
+              <button
+                className="btn btn-primary"
+                onClick={() => translate(ACTIONS.TRANSLATE.DOWN)}
+              >
+                Down
+              </button>
+            </div>
+          </div>
+        </div>
+      ) : null}
+    </div>
+  );
+}
